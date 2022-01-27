a9261dacf4b8390556821f5226a592c3bc9114f2
makucg <chengokuu@gmail.com>
2022-01-27 17:59:38 +0100

    Update Spring Boot versión. Reformat code.

--- src/main/java/com/refactorizando/hexagonalarchitecture/domain/User.java
+++ src/main/java/com/refactorizando/hexagonalarchitecture/domain/User.java
@@ -4,8 +4,6 @@ package com.refactorizando.hexagonalarchitecture.domain;
 public class User {
 
   private Long id;
-
   private String name;
-
   private String address;
 }
--- src/main/java/com/refactorizando/hexagonalarchitecture/infrastructure/config/spring/SpringBootServiceConfig.java
+++ src/main/java/com/refactorizando/hexagonalarchitecture/infrastructure/config/spring/SpringBootServiceConfig.java
@@ -1,18 +1,16 @@
 package com.refactorizando.hexagonalarchitecture.infrastructure.config.spring;
 
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import com.refactorizando.hexagonalarchitecture.application.repository.UserRepository;
 import com.refactorizando.hexagonalarchitecture.application.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringBootServiceConfig {
 
-
-  @Bean
-  public UserService userService(UserRepository userRepository) {
-    return new UserService(userRepository);
-  }
+    @Bean
+    public UserService userService(UserRepository userRepository) {
+        return new UserService(userRepository);
+    }
 }
